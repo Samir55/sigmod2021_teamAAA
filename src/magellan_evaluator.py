cdae5db02efb8561a0941879d9172ade2c3a0b7f
Ahmed Samir Abdelreheem <discere044@icloud.com>
2021-04-06 07:04:12 +0300

    adding some data cleaning

--- src/magellan_evaluator.py
+++ src/magellan_evaluator.py
@@ -4,33 +4,112 @@ import pandas as pd
 import os
 import joblib
 import sys
+import re
+
+
+def preprocess_laptop_dataset(df):
+    # Alpha numeric
+    irrelevant_regex = re.compile(r'[^a-z0-9.\-\s]')
+    multispace_regex = re.compile(r'\s\s+')
+
+    for column in df.columns:
+        df[column] = df[column].str.lower().str.replace(irrelevant_regex, ' ').str.replace(multispace_regex, ' ')
+
+    # Count the number of nans in a certain row and remove records with more than 3 nans
+    nans_count = df.isnull().sum(axis=1)
+    mask = nans_count > 3
+    print("removing {} records containing nans".format(len(df[mask])))
+    print(df[mask].head())
+    df = df[~mask]
+
+    # Brand assignment
+    all_brands = set()
+    extra_brands = set(pd.read_csv('laptops.csv').Company.str.lower().unique())
+    all_brands.update(extra_brands)
+
+    def assign_brand(record):
+        # Search in brand first
+        if record['brand'] in all_brands:
+            return record['brand']
+        # then in the title
+        for el in all_brands:
+            if el in record['title']:
+                return el
+        return "NNN"
+
+    df['brand'] = df.apply(assign_brand, axis=1)
+
+    # cpu brand
+    def assign_cpu_brand(record):
+        # Search in brand first
+        if 'intel' in str(record['cpu_brand']) or 'intel' in str(record['title']) or \
+                'intel' in str(record['cpu_model']) or 'intel' in str(record['cpu_type']):
+            return 'intel'
+        return 'amd'
+
+    df['cpu_brand'] = df.apply(assign_cpu_brand, axis=1)
+
+    # cpu model
+    def assign_cpu_model(record):
+        if record['cpu_brand'] == 'intel':
+            pass
+        else:
+            pass
+
+    # ram capacity
+    def assign_ram_capacity(record):
+        s = str(record['ram_capacity']).replace(' ', '')
+        possible_vals = ['2gb', '4gb', '6gb', '8gb', '10gb', '12gb', '16gb',
+                         '32gb', '64gb', '128gb', '256gb', '512gb', '2', '4',
+                         '6', '8', '10', '12', '16', '32', '64', '128']
+        for val in possible_vals:
+            if val in s:
+                return int(val.replace('gb', ''))
+
+        s = str(record['title']).replace(' ', '')  # This will be wrong, please change
+        possible_vals = ['2gb', '4gb', '6gb', '8gb', '10gb', '12gb', '16gb',
+                         '32gb', '64gb', '128gb']
+        for val in possible_vals:
+            if val in s:
+                return int(val.replace('gb', ''))
+
+        return 0
+
+    df['ram_capacity'] = df.apply(assign_ram_capacity, axis=1)
+
+    # Unit stand. in weight
+
+    # Save the cleaned dataset
+    df.to_csv('X_cleaned.csv', index=False)
+
 
 if __name__ == '__main__':
     # Get the arguments
     n = len(sys.argv)
-    print("Total arguments passed:", n)
-
-    # Arguments passed
-    print("\nName of Python script:", sys.argv[0])
-
-    print("\nArguments passed:", end=" ")
-    for i in range(1, n):
-        print(sys.argv[i], end=" ")
-
-    test_csv_file = sys.argv[1]
+    # print("Total arguments passed:", n)
+    #
+    # # Arguments passed
+    # print("\nName of Python script:", sys.argv[0])
+    #
+    # print("\nArguments passed:", end=" ")
+    # for i in range(1, n):
+    #     print(sys.argv[i], end=" ")
+    #
+    # test_csv_file = sys.argv[1]
 
     # Read the dataset / Clean / Save
-    A = pd.read_csv(test_csv_file)
+    A = pd.read_csv("X2.csv")
     A = A.fillna(-999)
-    A.to_csv('X_cleaned.csv', index=False)
+    preprocess_laptop_dataset(A)
+    sys.stderr.write("Reading Files Succeeded\n")
 
     # Reread the cleaned dataset
     A = em.read_csv_metadata('X_cleaned.csv', key='instance_id')
     B = em.read_csv_metadata('X_cleaned.csv', key='instance_id')
 
-    print('Number of tuples in A: ' + str(len(A)))
-    print('Number of tuples in B: ' + str(len(B)))
-    print('Number of tuples in A X B (i.e the cartesian product): ' + str(len(A) * len(B)))
+    sys.stderr.write('Number of tuples in A: ' + str(len(A)) + '\n')
+    sys.stderr.write('Number of tuples in B: ' + str(len(B)) + '\n')
+    sys.stderr.write('Number of tuples in A X B (i.e the cartesian product): ' + str(len(A) * len(B)) + '\n')
 
     # Start running blocking
     ob = em.OverlapBlocker()
