84f1c59eda886fc76562f5eb45f4c33721fec593
Ahmed Samir Abdelreheem <discere044@icloud.com>
2021-04-30 18:34:30 +0300

    refactor and add check_score notebook

--- src/record_linkage/eval_all_local.py
+++ src/record_linkage/eval_all_local.py
@@ -12,7 +12,7 @@ from record_linkage_dedupe_trainer_x4 import clean_products_dataset_dedupe as cl
 from record_linkage_dedupe_trainer_x4 import clean_products_dataset_rf as clean_x4_rf
 
 LOCAL = True
-NUM_CORES = 8
+NUM_CORES = 77
 
 partition_threshold = {
     'x2': 0.3,
@@ -21,14 +21,10 @@ partition_threshold = {
 }
 
 
-# Notes
-# Sub 2 and sub 3 are different only on x4 model
-# sub3 0.37 --> F: 63.9, P: 52, R: 80
-
 def deduper_eval(dataset_type: str, dataset):
     # Create deduper model
     with open('../../trained_models/deduper/sub_5/trained_{}_settings.json'.format(dataset_type), 'rb') as fin:
-        deduper = dedupe.StaticDedupe(fin, num_cores=NUM_CORES)
+        deduper = dedupe.StaticDedupe(fin, num_cores=8)
 
     # Prepare the data
     if dataset_type in ['x2', 'x3']:
@@ -68,11 +64,8 @@ def deduper_eval(dataset_type: str, dataset):
 
 
 def eval_lightgbm_dedupe(dataset_type: str, dataset_rf, dataset_dedupe):
-    #
-    # Get dedupe features
-    #
     # Create deduper model
-    with open('../../trained_models/combined/trained_{}_settings.json'.format(dataset_type), 'rb') as fin:
+    with open('../../trained_models/combined_2/trained_{}_settings.json'.format(dataset_type), 'rb') as fin:
         deduper = dedupe.StaticDedupe(fin, num_cores=NUM_CORES)
 
     cols = ['name', 'brand', 'size', 'product_type']
@@ -84,30 +77,31 @@ def eval_lightgbm_dedupe(dataset_type: str, dataset_rf, dataset_dedupe):
     clustered_dupes = deduper.partition(to_dedupe_dict, partition_threshold[dataset_type])
     print('# duplicate sets', len(clustered_dupes))
 
-    #
-    # Get the recordlinkage
-    #
+    # Create the record linkage model
+    # Indexer
     indexer = rl.Index()
     indexer.add(rl.index.Block('product_type'))
     indexer.add(rl.index.Block('brand'))
     indexer.add(rl.index.Block('size'))
-
     candidate_links = indexer.index(dataset_rf)
 
+    # Comparing
     compare_cl = rl.Compare(n_jobs=-1)
     compare_cl.exact('brand', 'brand')
     compare_cl.exact('product_type', 'product_type')
     compare_cl.exact('size', 'size')
-    # compare_cl.string('price', 'price')
     compare_cl.string('name', 'name', method='qgram')
     compare_cl.string('name', 'name', method='damerau_levenshtein')
     compare_cl.string('name', 'name', method='levenshtein')
-    compare_cl.string('name', 'name', method='smith_waterman')
     compare_cl.string('name', 'name', method='jarowinkler')
+    compare_cl.string('name', 'name', method='smith_waterman')
     compare_cl.string('name', 'name', method='lcs')
+    # compare_cl.string('price', 'price')
 
+    # Features
     features = compare_cl.compute(candidate_links, dataset_rf)
 
+    # Add dedupe features
     features['xy_same_entity'] = pd.Series(np.zeros(len(features)))
     features.xy_same_entity = 0.0
 
@@ -126,19 +120,21 @@ def eval_lightgbm_dedupe(dataset_type: str, dataset_rf, dataset_dedupe):
                     features.loc[r_k, 'xy_same_entity'] = p
 
     # Now load the lightgbm
-    bst = lgb.Booster(model_file='../../trained_models/combined/x4_lgb_classifier.txt')
+    bst = lgb.Booster(model_file='../../trained_models/combined_2/x4_lgb_classifier.txt')
 
-    labels = bst.predict(features)
-    features['labels'] = labels
+    # Predict
+    confs = bst.predict(features)
+    features['label'] = confs
 
-    preds = features[features['labels'] > 0.5]
-
+    # Save the csv file
     # Now export the left and right instance ids
     # Save the result
     res = []
-    for i in range(len(preds)):
-        record = preds.iloc[i]
-        res.append(record.name)
+    for i in range(len(confs)):
+        record = features.iloc[i]
+        label = record.label
+        if label > 0.5:
+            res.append(record.name)
 
     res_df = pd.DataFrame(res)
     res_df.columns = ['left_instance_id', 'right_instance_id']
@@ -185,14 +181,14 @@ if __name__ == '__main__':
 
     # Now, we evaluate based on the trained models
     print("Cleaning X2 dataset")
-    # x2 = clean_x2(x2)
+    x2 = clean_x2(x2)
     print("Evaluating X2 dataset")
-    # output = output.append(deduper_eval('x2', x2))
+    output = output.append(deduper_eval('x2', x2))
 
     print("Cleaning X3 dataset")
-    # x3 = clean_x3(x3)
+    x3 = clean_x3(x3)
     print("Evaluating X3 dataset")
-    # output = output.append(deduper_eval('x3', x3))
+    output = output.append(deduper_eval('x3', x3))
 
     print("Cleaning X4 dataset")
     x4_rf = clean_x4_rf(x4)
